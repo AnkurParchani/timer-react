bdd1a7430f9001a1f5a678dc2b96066c763b66b3
Ankur Parchani <ankurparchani@gmail.com>
2023-05-22 16:37:42 +0530

    set up wakelock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,23 @@ import InputForm from "./InputTimerForm/InputForm";
 import Heading from "./Heading";
 
 export default function App() {
+  const requestWakeLock = async () => {
+    try {
+      // Starting wakeLock for the first time
+      wakeLock = await navigator.wakeLock.request("screen");
+
+      // Checking if the user has left the page and returned again, then starting wakelock again
+      document.addEventListener("visibilitychange", async () => {
+        if (document.visibilityState === "visible" && wakeLock.released) {
+          wakeLock = await navigator.wakeLock.request("screen");
+        }
+      });
+    } catch (err) {
+      console.error(`${err.name}, ${err.message}`);
+    }
+  };
+  requestWakeLock();
+
   return (
     <div>
       <Heading />
